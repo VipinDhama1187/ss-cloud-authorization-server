a1e811a55cbce0dee333f2eae9d5fc575a8c9c9a
13400520 <a13400520@MAC13400520.local>
2022-01-15 09:04:18 +0530

    initial commit for authorization server

---
+++ src/main/java/com/vipin/authorization/server/SsCloudAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.vipin.authorization.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsCloudAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsCloudAuthorizationServerApplication.class, args);
+	}
+
+}
